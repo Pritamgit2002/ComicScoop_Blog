7a4f35051ada01ae6b6f28df458ec0d22bcb0879
Pritam Gain <pritamjob2021@gmail.com>
2024-02-24 20:01:59 +0530

    fix: issue

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,55 @@
 import { client, urlFor } from "@/app/lib/sanity";
 import { simpleBlogCard } from "@/app/lib/types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LayoutGrid } from "./layout-grid";
 
-async function getData() {
-  const query = `*[_type== 'blog' ] | order(_createdAt desc){
-      id,
-      title,
-      smallDescription,
-      "currentSlug":slug.current,
-      titleImage,
-    }`;
-  const data = await client.fetch(query);
-  console.log(data);
-  return data;
-}
-export default async function Card() {
-  const data: simpleBlogCard[] = await getData();
-  console.log(data);
+export default function Card() {
+  const [search, setSearch] = useState<string>("all");
+
+  const [data, setData] = useState<simpleBlogCard[]>([]);
+  useEffect(() => {
+    async function getData() {
+      let query = `*[_type == 'blog'`;
+
+      // If search is not 'all', add genre filter to the query
+      if (search !== "all") {
+        query += ` && genre == "${search}"`;
+      }
+
+      query += `] | order(_createdAt desc) {
+        id,
+        title,
+        smallDescription,
+        "currentSlug": slug.current,
+        titleImage,
+      }`;
+
+      try {
+        const result = await client.fetch(query);
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    getData();
+  }, [search]);
+  // const data: simpleBlogCard[] = await getData();
+  // console.log(data);
   return (
     <div className="min-h-screen h-max w-full overflow-y-auto bg-black   ">
+      <div className="w-full flex items-center justify-center gap-4 ">
+        {/* {content.map((item, i) => (
+          <div
+            className="text-2xl font-bold text-white cursor-pointer active:opacity-80 "
+            onClick={() => {
+              setSearch(item.genre);
+            }}
+          >
+            {item.genre}
+          </div>
+        ))} */}
+      </div>
       <LayoutGrid cards={data} />
     </div>
   );
