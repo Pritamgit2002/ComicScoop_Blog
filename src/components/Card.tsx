c8feb152ce3aabab1f69b6cc985fe63a1fb5bf86
Pritam Gain <pritamjob2021@gmail.com>
2024-02-24 13:31:26 +0530

    refrcator

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,12 +2,7 @@ import { client, urlFor } from "@/app/lib/sanity";
 import { simpleBlogCard } from "@/app/lib/types";
 import React from "react";
 import { LayoutGrid } from "./layout-grid";
-const content = [
-  { title: "all", link: "all" },
-  { title: "travel", link: "travel" },
-  { title: "sports", link: "sports" },
-  { title: "science", link: "science" },
-];
+
 async function getData() {
   const query = `*[_type== 'blog' ] | order(_createdAt desc){
       id,
@@ -25,11 +20,6 @@ export default async function Card() {
   console.log(data);
   return (
     <div className="min-h-screen h-max w-full overflow-y-auto bg-black   ">
-      <div className="w-full flex items-center justify-center gap-4 ">
-        {content.map((item, i) => (
-          <div>{item.title}</div>
-        ))}
-      </div>
       <LayoutGrid cards={data} />
     </div>
   );
